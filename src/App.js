9b54f1da019a18ecf998247def6b9ac0fafbea2e
leonardo <leonardoberon33@gmail.com>
2022-11-10 11:17:00 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import ItemListContaine from "./components/ItemListContaine";
+import './App.css';
+import React from 'react';
 
-const App = () => {
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { Navbar } from './components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {ItemDetailContainer} from "./components/ItemDetailContainer/ItemDetailContainer"
+function App() {
   return (
-    <>
-      <NavBar />
-      <ItemListContaine nombre= "Nueva Coleccion" />
-    </>
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<ItemListContainer/>}/>
+            <Route path="/category/:categoryId" element={<ItemListContainer/>}/>
+            <Route path="/item/:id" element={<ItemDetailContainer/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
